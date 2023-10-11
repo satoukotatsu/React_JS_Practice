90d90696c5e60b92bfc4245aaf3fc531ca4e9dbe
IQ5000 <50645516+satoukotatsu@users.noreply.github.com>
2023-10-11 13:54:51 +0000

    テンプレート文字列は便利

--- src/index.mjs
+++ src/index.mjs
@@ -2,17 +2,34 @@
  * const let などの変数宣言
  */
 
-let val2 = "let変数";
-console.log(val2);
+// let val2 = "let変数";
+// console.log(val2);
 
-val2 = "上書き可能";
-console.log(val2);
+// val2 = "上書き可能";
+// console.log(val2);
 
-const val3 = {
-  name: "kaniba",
-  age: 28,
-};
-console.log(val3);
-val3.name = "onion";
+// const val3 = {
+//   name: "kaniba",
+//   age: 28,
+// };
+// console.log(val3);
+// val3.name = "onion";
 
-console.log(val3);
+// console.log(val3);
+
+/**
+ * テンプレート文字列
+ */
+
+// const name = "IQ5000";
+// const age = 28;
+
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// // テンプレート文字列を用いると・・・
+// const message2 = `私の名前は${name} です。年齢は${age}です。`;
+// console.log(message2);
+
+// 
